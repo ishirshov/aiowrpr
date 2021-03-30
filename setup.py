a03f5374f03bbd80ae654c88aabc7a7547eabcea
ishirshov <4211798+ishirshov@users.noreply.github.com>
2021-03-31 01:01:23 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import aiowrpr
 
 INSTALL_REQUIRES = [
     'aiodns==2.0.0',
-    'aiohttp[speedups]==3.6.2',
+    'aiohttp[speedups]>=3.7.4',
     'aiohttp-apispec==2.1.0',
     'apispec==3.2.0',
     'async-timeout==3.0.1',
